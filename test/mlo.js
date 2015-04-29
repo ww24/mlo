768ef434b4788644cd03230bda9d8d479f148b85
Takenori Nakagawa <ww24gm@gmail.com>
2015-04-30 00:30:31 +0900

    Implement pattern matching

--- test/mlo.js
+++ test/mlo.js
@@ -6,19 +6,44 @@
 var mlo = require("../lib");
 
 var expect = require("chai").expect;
+var path = require("path");
 
 describe("mlo", function () {
   describe("basic", function () {
     it("no arguments", function () {
       var libs = mlo("../examples").load();
+
+      expect(Object.keys(libs)).to.have.length(3);
       expect(libs).to.have.property("module_a", "module_a.js");
       expect(libs).to.have.property("module_b", "module_b.js");
       expect(libs).to.have.property("mod-c", "mod-c.js");
     });
+
+    it("absolute path", function () {
+      var absolute_path = path.resolve(__dirname, "../examples");
+      var libs = mlo(absolute_path).load();
+
+      expect(Object.keys(libs)).to.have.length(3);
+      expect(libs).to.have.property("module_a", "module_a.js");
+      expect(libs).to.have.property("module_b", "module_b.js");
+      expect(libs).to.have.property("mod-c", "mod-c.js");
+    });
+
+    it("cannot load caller module", function () {
+      var libs = mlo(__dirname).load("mlo");
+
+      expect(Object.keys(libs)).to.have.length(0);
+    });
   });
 
   describe("pattern", function () {
+    it("module_*", function () {
+      var libs = mlo("../examples").load("module_*");
 
+      expect(Object.keys(libs)).to.have.length(2);
+      expect(libs).to.have.property("module_a", "module_a.js");
+      expect(libs).to.have.property("module_b", "module_b.js");
+    });
   });
 
   describe("recursively", function () {
